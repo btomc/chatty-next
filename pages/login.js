3a5b057b52010f63ea1de5503b96860b8a47ccb5
Beata Tomczak <tma.btty@gmail.com>
2021-04-20 23:59:39 +0200

    Build Login Screen

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,47 @@
-function login() {
+import styled from 'styled-components'
+import Head from 'next/head'
+import { Button } from '@material-ui/core'
+import { auth, provider } from '../firebase'
+
+function Login() {
+    const signIn = () => {
+        auth.signInWithPopup(provider).catch(alert)
+    }
+
     return (
-        <div>
-            login
-        </div>
+        <Container>
+            <Head>
+                <title>Login</title>
+            </Head>
+
+            <LoginContainer>
+                <Logo src='/logo.png' />
+                <Button onClick={signIn} variant='outlined'>Sign in with Google</Button>
+            </LoginContainer>
+        </Container>
     )
 }
 
-export default login
+export default Login
+
+const Container = styled.div`
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    background: whitesmoke;
+`
+const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 100px;
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    box-shadow: 0px 4px 14px -3px rgba(0,0,0,0.7);
+`
+
+const Logo = styled.img`
+    width: 200px;
+    height: 200px;
+    margin-bottom: 50px;
+`
