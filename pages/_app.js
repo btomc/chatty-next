1ed667bcead918881af93231ddeb76a816ce017a
Beata Tomczak <tma.btty@gmail.com>
2021-04-21 14:34:45 +0200

    Capture and store user details

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
+import React, {useEffect} from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebase'
 import Login from '../pages/login'
 import Loading from './components/Loading'
+import firebase from 'firebase'
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    if(user) {
+      db.collection('users').doc(user.uid).set({
+        email: user.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        photoURL: user.photoURL
+      }, { merge: true })
+    }
+  }, [user])
 
   if (loading) return <Loading />
   if(!user) return <Login />
