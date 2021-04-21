83d3081ccd741b0dfdbd445cc08455381c4059ca
Beata Tomczak <tma.btty@gmail.com>
2021-04-21 15:07:12 +0200

    Sidebar - sign out, create chat, fetch list of chats

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -4,23 +4,41 @@ import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchIcon from '@material-ui/icons/Search'
 import * as EmailValidator from 'email-validator'
+import { auth, db } from '../../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import Chat from './Chat'
 
 function Sidebar() {
+    const [user] = useAuthState(auth)
+    const userChatRef = db.collection('chats').where('users', 'array-contains', user.email)
+    const [chatsSnapshot] = useCollection(userChatRef)
+
     const createChat = () => {
         const input = prompt(
             'Please enter an email address for the user you wish to chat with'
         )
         if(!input) return null
 
-        if (EmailValidator.validate(input)) {
-            // Add the chat into the DB 'chats'
+        if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email) {
+            // Add the chat into the DB 'chats', check if it doesn't exist and is valid
+            db.collection('chats').add({
+                users: [user.email, input],
+            })
         }
     }
+
+    const chatAlreadyExists = (recipientEmail) => 
+        !!chatsSnapshot?.docs.find(
+            chat => 
+                chat.data().users.find(user => user === recipientEmail)?.length > 0
+        )
+    
 
     return (
         <Container>
             <Header>
-                <UserAvatar />
+                <UserAvatar onClick={() => auth.signOut()} />
                 <IconsContainer>
                     <IconButton>
                         <ChatIcon style={{ color: '#fff'}} />
@@ -41,6 +59,9 @@ function Sidebar() {
             </SidebarButton>
 
             {/* List of Chats */}
+            {chatsSnapshot?.docs.map(chat => (
+                <Chat key={chat.id} id={chat.id} user={chat.data().users} />
+            ))}
         </Container>
     )
 }
